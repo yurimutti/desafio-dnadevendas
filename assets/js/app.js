077312d46949c3a23beb5ab4fd0b6d9f6915576d
yuri.m <muttiyuri@gmail.com>
2021-06-03 19:28:51 -0300

    finalizando responsividade

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,28 @@
-
 // Swiper 
 const swiper = new Swiper('.slider', {
-
+    navigation: {
+        nextEl: '.slider-next',
+        prevEl: false,
+    },
+    simulateTouch:false,
     breakpoints: {
         1200: {
+            loop: false,
             slidesPerView: 4,
-            allowSlidePrev: false,
-            allowSlideNext: false,
         },
         990: {
-            loop: true, 
-            navigation: {
-                nextEl: '.slider-next',
-                prevEl: false,
-            },
-            allowSlidePrev: true,
-            allowSlideNext: true,
+            loop: true,
             slidesPerView: 3,
         },
-        720: {
-            loop: true, 
-            navigation: {
-                nextEl: '.slider-next',
-                prevEl: false,
-            },
-            allowSlidePrev: true,
-            allowSlideNext: true,
+        790: {
+            loop: true,
             slidesPerView: 2,
         },
         320: {
-            loop: true, 
-            navigation: {
-                nextEl: '.slider-next',
-                prevEl: false,
-            },
-            allowSlidePrev: true,
-            allowSlideNext: true,
+                loop: true,
             slidesPerView: 1,
         },
     }
 });
+
+
